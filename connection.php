1783e4825fe5dc9dbfdddcc81e3137f99666d1e8
Blake <ama542blake@gmail.com>
2019-02-26 14:17:34 -0500

    Connection works correctly

--- connection.php
+++ connection.php
@@ -2,6 +2,7 @@
     $db_username = "root";
     $db_password = "";
     $db_name = "music_site";
+    // use $conn in mysqli_query
     $conn = mysqli_connect("localhost", $db_username, $db_password, $db_name);
     
     if ($conn->connect_error) {
