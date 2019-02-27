4beb2814d78ba32a2f2dc251bd8c26cb6cea9794
Blake <ama542blake@gmail.com>
2019-02-26 22:11:12 -0500

    decided to redirect on submit

--- add_artist.php
+++ add_artist.php
@@ -1,24 +1,19 @@
 <?php 
     include_once('includes/header.php');
     include_once('includes/connection.php');
-    
 ?>
-    
-<!-- the blank iframe is used as a target so that when the form is submitted, the user isn't redirected to a new page, rather
-     the data is sent to add_artist_to_db.php, which simply inserts the entry into the database without need to display a new page  -->
-<iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
-<form action="add_artist_to_db.php" method="post" >
+
+<form action="add_artist_to_db.php" method="post">
     <div class="form-group">
         <label>Artist name: 
-            <input class="form-control" type="text" name="artist_name" target="dummyframe" required>
+            <input class="form-control" type="text" name="artist_name" required>
         </label>
         <label>Is this a solo artist or band? (has more than 1 member)?
             <input id="solo_radio"class="form-check" type="radio" name="isband" value="0" required checked>Solo
             <input id="band_radio" class="form-check" type="radio" name="isband" value="1" required>Band
         </label>
-        <input type="submit">
+        <input type="submit"> 
     </div>
-
 </form>
 
 
--- add_artist_to_db.php
+++ add_artist_to_db.php
@@ -1,12 +1,12 @@
 <?php
     include_once('includes/connection.php');
-
-echo "NAME ${_POST['artist_name']}";
-echo "ISBAND ${_POST['isband']}";
     if (isset($_POST['artist_name'])) {
         $name = $_POST['artist_name'];
         $isband = $_POST['isband'];
         $query = "INSERT INTO artist (artist_name, artist_is_band) VALUES ('${name}', '${isband}')";
         mysqli_query($conn, $query);
+        echo "Artist added successfully, page will redirect after 5 seconds";
+        sleep(5);
+        header("Location: index.php");
     }
 ?>
