93645247a6d5e193cd08cec6eb846df3d7dfb7ad
Blake <ama542blake@gmail.com>
2019-03-15 14:59:56 -0400

    Backing up last few bits of work

--- display_song.php
+++ display_song.php
@@ -1,9 +1,28 @@
 <?php
     include_once("includes/header.php");
     include_once("includes/connection.php");
-
-    var_dump($_GET);
+    include_once("includes/common_query.php");
     
-
+    // make sure the essential variables are set
+    if ((isset($GET_['song_id'])) && (isset($GET_['album_id'])) &&(isset($GET_['artist_id']))) {
+        $songID = $_GET['song_id'];
+        $albumID = $_GET['album_id'];
+        $artistID = $_GET['artist_id'];
+        
+        if (isset($_GET['producerID'])) {
+            $producerID = $_GET['producer_id'];
+        } else {
+            $producerID = NULL;
+        }
+        if (isset($_GET['genreID'])) {
+            $genreID = $_GET['genre_id'];
+        } else {
+            $genreID = NULL;
+        }
+        
+        //TODO get the names for each of the variables that have IDs, use common_query.php for this
+        
+    }
+    
     include_once("includes/footer.php");
 ?>
