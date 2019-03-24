2b5958ffed4ebc47dacac919abd044dfca5c78b7
Chad Czerny <cmczerny@oakland.edu>
2019-03-24 15:23:52 -0400

    Re-Styled Login/Signup Page

--- login_signup.php
+++ login_signup.php
@@ -2,66 +2,55 @@
     include_once('includes/header.php');
 ?>
 
-<div class="container">
-    <div class="card bg-warning mt-5" id="login">
-        <div class="card-header">Log In</div>
-        <div class="card-body">
-            <div class="form-group container">
-                <form action="login.php">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <label class='form-label'>Username
-                                <input class="ml-1 form-control" type="text" name="username" required>
-                            </label>
-                        </div>
-                        <div class="col-sm-6">
-                            <label class='form-label align-self-end'>Password
-                                <input class="ml-1 form-control" type="text" name="password" required>
-                            </label>
-                        </div>
-                        <div class="col-sm-6">
-                            <input class="btn btn-outline-dark" type="submit" value="Log In">
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    </div>
-
-    <div class="card bg-warning mt-5" id="signup">
-        <div class="card-header">Create an account</div>
-        <div class="card-body">
-            <div class="form-group container">
-                <form action="signup.php">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <label class='form-label'>Select a username:
-                                <input class="ml-1 form-control" type="text" name="username" required>
-                            </label>
-                        </div>
-                        <div class="col-sm-6">
-                            <label class='form-label'>Select a password:
-                                <input class="ml-1 form-control" type="text" name="password" required>
-                            </label>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <label class='form-label'>Enter your email address:
-                                <input class="ml-1 form-control" type="email" name="email" required>
-                            </label>
-                        </div>
-                        <div class="col-sm-6">
-                            <input class="btn btn-outline-dark" type="submit" value="Create account">
-                        </div>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    </div>
+<div class="container p-0 text-center">
+ <form class="displayBlock jumbotron logInForm pagination-centered p-0" action="login.php">
+  <div class="card logInHeader p-3">
+   <h1>Log-In</h1>
+  </div>
+  <div class="p-3">
+   <div>
+    <label class='form-label'><h5>Username</h5>
+     <input class="ml-1 form-control m-2" type="text" name="username" required>
+    </label>
+   </div>
+   <div>
+    <label class='form-label align-self-end'><h5>Password</h5>
+     <input class="ml-1 form-control m-2" type="text" name="password" required>
+    </label>
+   </div>
+   <div>
+    <input class="btn btn-outline-dark m-3" type="submit" value="Log In">
+   </div>
+  </div>
+ </form>
+ 
+ <form class="displayBlock jumbotron logInForm pagination-centered p-0" action="signup.php">
+  <div class="card logInHeader p-3">
+   <h1>Sign-up</h1>
+  </div>
+  <div class="p-3">
+   <div>
+    <label class='form-label'><h5>Select a username:</h5>
+     <input class="ml-1 form-control" type="text" name="username" required>
+    </label>
+   </div>
+   <div>
+    <label class='form-label'><h5>Select a password:</h5>
+     <input class="ml-1 form-control" type="text" name="password" required>
+    </label>
+   </div>
+   <div>
+    <label class='form-label'><h5>Enter your email address:</h5>
+     <input class="ml-1 form-control" type="email" name="email" required>
+    </label>
+   </div>
+   <div>
+    <input class="btn btn-outline-dark" type="submit" value="Create account">
+   </div>
+  </div>
+ </form>
 </div>
+
 
 
 <hr id="vr">
