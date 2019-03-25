92de808c3907ac9542d0da93458fe7b80b853eca
Chad Czerny <cmczerny@oakland.edu>
2019-03-25 09:38:04 -0400

    User can log in

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
     session_start();
     include_once('includes/connection.php');
     include_once('includes/header.php');
+    include_once('includes/common_query.php');
     
     if ((isset($_POST['username'])) && (isset($_POST['password']))) {
         $username = $_POST['username'];
@@ -10,12 +11,18 @@
         if ($userID = userExists($conn, $username)) {
             if (userPasswordIsCorrect($conn, $userID, $password)) {
                 $_SESSION['username'] = $username;
+                header( "refresh:2; url=index.php" );
+                echo "Welcome {$username}. Redirecting...";
+                exit;
             }
         } else {
-            header("Location: login_signup.php");
+            header( "refresh:2; url=login_signup.php" );
+            echo "Incorrect credentials. Redirecting...";
+            exit;
         }
     }
      else {
-        echo "please enter credentials";
+        echo "Please enter credentials";
+        header( "refresh:2; url=login_signup.php" );
     }
 ?>
