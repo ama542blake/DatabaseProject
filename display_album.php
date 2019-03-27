49c746b0953a19c2dfc917e962f712f9fdc29389
Blake <ama542blake@gmail.com>
2019-03-27 11:04:25 -0400

    Fixed album artwork artist display on display_album
    Also added some js for edit album

--- display_album.php
+++ display_album.php
@@ -10,7 +10,11 @@
         $albumName = getAlbumName($conn, $albumID);
         $albumYear = getAlbumYear($conn, $albumID);
         $albumArtworkArtistID = getAlbumArtworkArtistID($conn, $albumID);
-        $albumArtworkArtistName = getArtworkArtistName($conn, $albumArtworkArtistID);
+        if ($albumArtworkArtistID) {
+            $albumArtworkArtistName = getArtworkArtistName($conn, $albumArtworkArtistID);
+        } else {
+            $albumArtworkArtistName = NULL;
+        }
         
         // get the list of artists associated with the album
         $albumArtistIDs = getArtistIDsFromArtistAlbum($conn, $albumID);
@@ -31,18 +35,18 @@
         echo "<h2>${albumName}</h2>";
         echo "<p>Year Released: <span id='albumYear'>${albumYear}</span></p><br>";
         // TODO eventually make a link that takes to a page that display all albums
-        // the artowrk artist has done art for (will require creation of diplay_artwork_artist.php, or something like it)
+        // the artwork artist has done art for (will require creation of diplay_artwork_artist.php, or something like it)
         echo "<p id='albumArtwork'>Artwork Artist: <span id='albumArtworkArtistName'.<a href=''>${albumArtworkArtistName}</a></span></p><br>";
         
         // display the artists        
-        echo "<p>By: </p>";
+        echo "<p>By: </p><span id='album-artists'>";
         $artists = array();
         for ($i = 0; $i < count($albumArtistNames); $i++) {
             $artists[$i] = "<a href='display_artist.php?artist_id=${albumArtistIDs[$i]}'>${albumArtistNames[$i]}</a>";
         }
         // this displays the links to the artists in a comma seperated list
         echo implode(", ", $artists);
-        echo "<br><br>";
+        echo "</span><br><br>";
         
         // display the songs
         echo "<p>Songs:</p><ul>";
