93b9d43fcb82740b75ac66c62e6225b9ade1ec04
Chad Czerny <cmczerny@oakland.edu>
2019-03-27 13:32:23 -0400

    Update update_query.php

--- includes/update_query.php
+++ includes/update_query.php
@@ -6,31 +6,47 @@
         //this will need to be updated to accommodate multiple later
 
     /* update queries */
-    function updateSong($conn, $songID, $producerID, $genreID) {
-        if (!($producerID)) {$producerID = "NULL";};
-        if (!($genreID)) {$genreID = "NULL";}
-        $query = "UPDATE song SET song_producer = ${producerID}, song_genre = ${genreID} WHERE song_id = ${songID}";
-        mysqli_query($conn, $query);
-    }
-
     function updateArtist($conn, $artistID, $artistIsBand) {
         $query = "UPDATE artist SET artist_is_band = ${artistIsBand} WHERE artist_id = ${artistID}";
         mysqli_query($conn, $query);
     }
     function updateAlbum ($conn, $albumID, $albumArtworkArtist, $albumReleasedYear){
         $query = "UPDATE album SET album_artwork_artist = ${albumArtworkArtist},  album_released_year = ${albumReleasedYear} WHERE album_id = ${albumID}";
+<<<<<<< HEAD
+=======
         mysqli_query($conn, $query);
     }
 
-    
+    function updateSong($conn, $songID, $producerID, $genreID) {
+        if (!($producerID)) {$producerID = "NULL";}
+        if (!($genreID)) {$genreID = "NULL";}
+        $query = "UPDATE song SET song_producer = ${producerID}, song_genre = ${genreID} WHERE song_id = ${songID}";
+>>>>>>> 71d923beb02bf0431dde97ba062c4e96dc39f8f8
+        mysqli_query($conn, $query);
+    }
+
     /* deletion quries */
+
+    function deleteArtistAlbum($conn, $albumID) {
+        $query = "DELETE FROM artist_album WHERE album_id = ${albumID}";
+        mysqli_query($conn, $query);
+    }
+    
     function deleteArtistSong($conn, $songID) {
         $query = "DELETE FROM artist_song WHERE song_id = ${songID}";
         mysqli_query($conn, $query);
     }
 
-    function deleteAlbumSong($conn, $songID) {
-        $query = "DELETE FROM album_song WHERE song_id = ${songID}";
+    /* idType specifies whether album ID ($idType === "album") is being used 
+    to delete, or song ID ($idType === "song"); */
+    function deleteAlbumSong($conn, $id, $idType) {
+        if ($idType === "song") {
+            $query = "DELETE FROM album_song WHERE song_id = ${id}";
+        } else if ($idType === "album") {
+            $query = "DELETE FROM album_song WHERE album_id = ${id}";
+        } else {
+            return;
+        }
         mysqli_query($conn, $query);
     }
 ?>
