e221c599fa3473b63f82d19a485cb3e06e9f8eb2
Blake <ama542blake@gmail.com>
2019-03-29 12:19:45 -0400

    Cleaned code up

--- search_results.php
+++ search_results.php
@@ -12,63 +12,33 @@
         $search_query = $_POST['searchquery'];
         
         if ($searchtype === 'artist') {
-            echo "<form id='artist_selection' action='show_artist.php' method='post'>";
-            echo "<input type='hidden' name='selected_id">
-<<<<<<< HEAD
-            search_artist($search_query);
-            
-=======
-            searchAlbum($search_query);
->>>>>>> 4d259d002cd9241dde51e1f85df03550a1c10575
+            echo "<form id='artist_selection' action='show_artist.php' method='post'>"
+               . "<input type='hidden' name='selected_id">;
+            searchAlbum($conn, $search_query);
             echo "</form>";
-            echo "artist fired";
         } else if ($searchtype === 'album') {
             echo "<form id='album_selection' action='show_album.php' method='post'>";
-<<<<<<< HEAD
-            search_album($search_query);
-            
-=======
-            searchAlbum($search_query);
->>>>>>> 4d259d002cd9241dde51e1f85df03550a1c10575
+            searchAlbum($conn, $search_query);
             echo "</form>";
-            echo "album fired";
         } else if ($searchtype === 'song') {
             echo "<form id='song_selection' action='show_song.php' method='post'>";
-<<<<<<< HEAD
-            search_song($search_query);
-            
-=======
             searchSong($conn, $search_query);
->>>>>>> 4d259d002cd9241dde51e1f85df03550a1c10575
             echo "</form>";
-            echo "song fired";
         } else {
-            echo "<div>";
-             echo "<h2>Artists</h2>";
-             echo "<form id='artist_selection' action='show_artist.php' method='post'>";
-             echo "<input type='hidden' name='selected_id'>";
-<<<<<<< HEAD
-
-             search_artist($search_query);
-=======
-             echo "dgdgdsfg";
+            echo "<div>" . "<h2>Artists</h2>" 
+               . "<form id='artist_selection' action='show_artist.php' method='post'>";
+               . "<input type='hidden' name='selected_id'>";
              searchArtist($conn, $search_query);
->>>>>>> 4d259d002cd9241dde51e1f85df03550a1c10575
-             echo "</form>";
-            echo "</div>";
-            echo "<div>";
-             echo "<h2>Albums</h2>";
-             echo "<form id='album_selection' action='show_album.php' method='post'>";
+             echo "</form>" . "</div>" . "<div>"
+                . "<h2>Albums</h2>"
+                . "<form id='album_selection' action='show_album.php' method='post'>";
              searchAlbum($conn, $search_query);
-             echo "</form>";
-            echo "</div>";
-            echo "<div>";            
-             echo "<form id='song_selection' action='show_song.php' method='post'>";
+             echo "</form>" . "</div>" . "<div>"
+                . "<form id='song_selection' action='show_song.php' method='post'>";
              searchSong($conn, $search_query);
-             echo "</form>";
-            echo "</div>"; 
+             echo "</form></div>"; 
         }
     }
     
-    include_once('includes/footer_search_results.php');
+    include_once('includes/footer.php');
 ?>
