f202ba1643162b64343d340a32e2effc134f2915
Blake <ama542blake@gmail.com>
2019-04-01 16:30:36 -0400

    Multiple updates to add_artist.php/js
    In addition to allowing users to indicate one band that a solo artist is in, they can now add multiple band memberships for that solo artist. Users may also now enter multiple band members for a band artist entry.

--- add_artist.php
+++ add_artist.php
@@ -11,14 +11,15 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- our own CSS -->
     <link rel="stylesheet" href="includes/main.css">
-	<link rel="icon" href="images/DB_logo_half.png">
+    <link rel="icon" href="images/DB_logo_half.png">
 </head>
 
 <?php
-        session_start();
-        include_once('includes/header.php');
+    session_start();
+    include_once('includes/header.php');
     include_once('includes/connection.php');
-    ?>
+?>
+
 <div id="artistImageDiv" class="addImageHeader bold image-fluid text-white text-right p-5 rounded">
     <h1>Add an Artist.</h1>
 </div>
@@ -32,6 +33,10 @@
             <h4>Is this a solo artist or band?</h4>
             <input id="solo_radio" class="form-check-inline ml-2 mr-1" type="radio" name="isband" value="0" required>Solo
             <input id="band_radio" class="form-check-inline ml-2 mr-1" type="radio" name="isband" value="1" required checked>Band
+        </label>
+        <label id="band-members-input" class="mb-3">Band Members (optional):
+            <input type="text" class="form-control input-info" name="band_members"><br>
+            <p>Seperate band names with a comma.</p>
         </label>
         <input type="submit" id="submit-artist" class="btn btn-outline-dark input-info">
     </div>
